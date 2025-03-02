7cf3184e52324ff1e5ffdcdf5bd400617bb560db
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-02 14:57:04 +0900

    feat: update posts

---
+++ prisma/migrations/20250302055304_update_posts/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Added the required column `content` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `emoji` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "content" TEXT NOT NULL,
+ADD COLUMN     "emoji" TEXT NOT NULL;
