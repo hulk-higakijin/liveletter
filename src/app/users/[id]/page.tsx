262cf03fbbd293af1c7a93d0d7e6c67c00f96166
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-02 18:26:16 +0900

    feat: edit button

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -2,11 +2,16 @@ import { api } from "~/trpc/server";
 import Layout from "~/app/layout";
 import UserAvatar from "~/users/components/UserAvatar";
 import PostCard from "~/posts/components/PostCard";
+import { GoPencil } from "react-icons/go";
+import Link from "next/link";
+import { auth } from "~/server/auth";
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 	const { id } = await params;
 	const { user } = await api.user.findOne({ id });
 	const { posts } = await api.post.whereByUserId({ id: user?.id || "" });
+	const session = await auth();
+	const isMyPage = session?.user?.id === user?.id;
 
 	if (!user) {
 		return <p>No user</p>;
@@ -18,6 +23,14 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 				<div className="flex gap-2 my-4">
 					<UserAvatar thumbnailUrl={user.image} />
 					<span className="my-auto">{user.name}</span>
+					{isMyPage && (
+						<Link
+							href={"/posts/new"}
+							className="btn btn-neutral btn-circle ml-auto"
+						>
+							<GoPencil className="text-lg" />
+						</Link>
+					)}
 				</div>
 
 				<div role="tablist" className="tabs tabs-bordered border-primary">
